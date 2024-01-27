185d1c38deaab72cc267c4ece26a30f314173962
Diogo Melo <diogo.antoniomelo@gmail.com>
2024-01-26 22:07:33 -0300

    Add & Update Product Types on the Client

--- BlazorEcommerce/Server/Controllers/ProductTypeController.cs
+++ BlazorEcommerce/Server/Controllers/ProductTypeController.cs
@@ -21,7 +21,21 @@ namespace BlazorEcommerce.Server.Controllers
         [HttpGet]
         public async Task<ActionResult<ServiceResponse<List<ProductType>>>> GetProductTypes()
         {
-            var response =  await _productTypeService.GetProductTypes();
+            var response = await _productTypeService.GetProductTypes();
+            return Ok(response);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<ServiceResponse<List<ProductType>>>> AddProductType(ProductType productType)
+        {
+            var response = await _productTypeService.AddProductType(productType);
+            return Ok(response);
+        }
+
+        [HttpPut]
+        public async Task<ActionResult<ServiceResponse<List<ProductType>>>> UpdateProductTypes(ProductType productType)
+        {
+            var response = await _productTypeService.UpdateProductType(productType);
             return Ok(response);
         }
     }
--- BlazorEcommerce/Server/Services/ProductTypeService/IProductTypeService.cs
+++ BlazorEcommerce/Server/Services/ProductTypeService/IProductTypeService.cs
@@ -5,5 +5,8 @@ namespace BlazorEcommerce.Server.Services.ProductTypeService
     public interface IProductTypeService
     {
         Task<ServiceResponse<List<ProductType>>> GetProductTypes();
+        Task<ServiceResponse<List<ProductType>>> AddProductType(ProductType productType);
+        Task<ServiceResponse<List<ProductType>>> UpdateProductType(ProductType productType);
+
     }
 }
